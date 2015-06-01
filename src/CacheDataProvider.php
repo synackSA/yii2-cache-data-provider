fb6cbe07046910d86942ce72c2b127d102092270
Garth Michel <synackSA>
2015-06-01 13:26:57 -0400

    Adding updated files and the actual php file

---
+++ src/CacheDataProvider.php
@@ -0,0 +1,44 @@
+
+<?php
+
+namespace synacksa\cachedataprovider;
+
+use yii\db\QueryInterface;
+use yii\base\InvalidConfigException;
+
+class CacheDataProvider extends \yii\data\ActiveDataProvider
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function prepareModels()
+    {
+        if (!$this->query instanceof QueryInterface) {
+            throw new InvalidConfigException('The "query" property must be an instance of a class that implements the QueryInterface e.g. yii\db\Query or its subclasses.');
+        }
+        $query = clone $this->query;
+        if (($pagination = $this->getPagination()) !== false) {
+            $pagination->totalCount = $this->getTotalCount();
+            $query->limit($pagination->getLimit())->offset($pagination->getOffset());
+        }
+        if (($sort = $this->getSort()) !== false) {
+            $query->addOrderBy($sort->getOrders());
+        }
+
+        \Yii::info("Trying to get query results");
+        return $this->db->cache(function ($db) use ($query) {
+            return $query->all($db);
+        });
+    }
+}
+
+?>
