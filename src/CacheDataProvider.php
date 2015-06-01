ffa69dfb3cf23f11de65a996e427e9d8ef35a063
Garth Michel <synackSA>
2015-06-01 14:45:55 -0400

    Fixed some bugs with the db connection

--- src/CacheDataProvider.php
+++ src/CacheDataProvider.php
@@ -1,10 +1,11 @@
-
 <?php
 
 namespace synacksa\cachedataprovider;
 
 use yii\db\QueryInterface;
 use yii\base\InvalidConfigException;
+use yii\di\Instance;
+use yii\db\Connection;
 
 class CacheDataProvider extends \yii\data\ActiveDataProvider
 {
@@ -25,17 +26,18 @@ class CacheDataProvider extends \yii\data\ActiveDataProvider
         if (!$this->query instanceof QueryInterface) {
             throw new InvalidConfigException('The "query" property must be an instance of a class that implements the QueryInterface e.g. yii\db\Query or its subclasses.');
         }
+
         $query = clone $this->query;
         if (($pagination = $this->getPagination()) !== false) {
             $pagination->totalCount = $this->getTotalCount();
             $query->limit($pagination->getLimit())->offset($pagination->getOffset());
         }
+
         if (($sort = $this->getSort()) !== false) {
             $query->addOrderBy($sort->getOrders());
         }
 
-        \Yii::info("Trying to get query results");
-        return $this->db->cache(function ($db) use ($query) {
+        return \Yii::$app->db->cache(function ($db) use ($query) {
             return $query->all($db);
         });
     }
