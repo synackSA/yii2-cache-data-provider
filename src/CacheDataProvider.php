dac65efcd255f7443d840a492a442f39f27e7107
Garth Michel <synackSA>
2015-06-16 12:14:45 -0400

    some of Jaslin changes implemented https://github.com/JasLin

--- src/CacheDataProvider.php
+++ src/CacheDataProvider.php
@@ -4,8 +4,6 @@ namespace synacksa\cachedataprovider;
 
 use yii\db\QueryInterface;
 use yii\base\InvalidConfigException;
-use yii\di\Instance;
-use yii\db\Connection;
 
 class CacheDataProvider extends \yii\data\ActiveDataProvider
 {
@@ -39,6 +37,30 @@ class CacheDataProvider extends \yii\data\ActiveDataProvider
 
         return \Yii::$app->db->cache(function ($db) use ($query) {
             return $query->all($db);
+        });
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function prepareTotalCount()
+    {
+        if(!$this->cache){
+            return parent::prepareTotalCount();
+        }
+        if (!$this->query instanceof QueryInterface) {
+            throw new InvalidConfigException('The "query" property must be an instance of a class that implements the QueryInterface e.g. yii\db\Query or its subclasses.');
+        }
+        $query = clone $this->query;
+
+        $db = $this->db;
+        if($db === null){
+            $modelClass = $this->query->modelClass;
+            $db = $modelClass::getDb();
+        }
+
+        return $db->cache(function($db) use($query){
+            return (int) $query->limit(-1)->offset(-1)->orderBy([])->count('*', $db);
         });
     }
 }
